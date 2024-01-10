9839dafb232860d629215583434fff31ceb6d1a1
Ganesh Rajan <ganesh@Ganeshs-MacBook-Air.local>
2024-01-10 23:05:11 +0530

    Added Party Mode

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-Built a simple Vanilla JS site to learn Containerisation with Docker. 
+### Simple Vanilla JS site to learn Containerization with Docker
 
-To run the app, download folder and cd to it, then run 
-docker compose up --build
-Will run on port 8089
+#### To run the app, download folder and cd to it, then run 
+#### docker compose up --build
+#### Will run on port 8089
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const bodyElement = document.getElementById("body");
+let intervalId;
+let partying = false;
 const colorArray = [
   "red",
   "green",
@@ -23,14 +25,33 @@ function randomizeColor() {
     newColor = colorArray[Math.floor(Math.random() * colorArray.length)];
   } while (newColor == bodyElement.style.backgroundColor);
   console.log("old is", bodyElement.style.backgroundColor);
-  console.log("new is ", newColor)
+  console.log("new is ", newColor);
   bodyElement.style.backgroundColor = newColor;
 }
 
 function changeColor(color) {
+  if (partying) {
+    stopParty();
+  }
   if (color != "random") {
     bodyElement.style.backgroundColor = color;
   } else {
     randomizeColor();
   }
+}
+
+function changeParty() {
+  if (!partying) {
+    partying = true;
+    intervalId = setInterval(randomizeColor, 200);
+    document.getElementById("party-button").innerHTML = "Relax";
+  } else {
+    stopParty();
+  }
+}
+
+function stopParty() {
+  document.getElementById("party-button").innerHTML = "Party!";
+  partying = false;
+  clearInterval(intervalId);
 }
